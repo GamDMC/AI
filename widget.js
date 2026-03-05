644dd210929adbb7fce8ba46f428525b049c27f5
GamDMC <blr_game@davincimediacollege.com>
2026-03-05 12:34:23 +0530

    Update widget.js

--- widget.js
+++ widget.js
@@ -1,6 +1,6 @@
 (function () {
 
-    const CHAT_URL = "https://gamdmc.github.io/AI/sp";
+    const CHAT_URL = "http://127.0.0.1:5500/spfine%20copy.html";
     const isMobile = window.innerWidth < 480;
 
     /* Floating Button */
